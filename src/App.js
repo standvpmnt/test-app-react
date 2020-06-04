5e59829b692845b590ca1252c134790a411399ef
STan Dvpmnt <standvpmnt@gmail.com>
2020-06-04 12:21:47 +0530

    Created home page and added styling for the home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import HomePage from "./homepage.component";
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
-
+// Also added node-sass using yarn in this process
 export default App;
