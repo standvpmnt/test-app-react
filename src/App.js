d7953cc9309779ca02f6ef7d12cd925a5528f766
STan Dvpmnt <standvpmnt@gmail.com>
2020-06-04 17:05:30 +0530

    adding withRouter to avoid props drilling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
-// Also added node-sass using yarn in this process
+// Also added react-router-dom using yarn
 export default App;
